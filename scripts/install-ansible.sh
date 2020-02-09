b4ba69df29da69781f5afdaabe0b88a133bde22a
r1cep <dev.r1cep@gmail.com>
2020-02-09 17:01:34 +0900

    :tada: Initial commit.

---
+++ scripts/install-ansible.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ref. https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installing-ansible-on-ubuntu
+
+set -eux
+
+sudo apt update
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible python-apt
